faa4354e705724cfbdefacef402fe42ad93059e8
gang <foodoon@qq.com>
2015-01-08 23:18:34 +0800

    add

---
+++ deploy/build.sh
@@ -0,0 +1,9 @@
+cd .. && git pull
+tomcat_home=/home/ec2-user/tomcat-fuwu
+mvn clean package -Dmaven.test.skip=true 
+mv target/fuwu-1.0-SNAPSHOT.war ROOT.war
+rm -rf ${tomcat_home}/webapps/ROOT.war
+rm -rf ${tomcat_home}/webapps/ROOT
+
+cp ROOT.war ${tomcat_home}/webapps/
+
