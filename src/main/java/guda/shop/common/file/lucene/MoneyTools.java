b8a2c2bbb192fcb3befc952126decdbd65f35cda
foodoon <foodoon@qq.com>
2015-01-31 10:18:16 +0800

    add

--- src/main/java/guda/shop/common/file/lucene/MoneyTools.java
+++ src/main/java/guda/shop/common/file/lucene/MoneyTools.java
@@ -4,16 +4,20 @@ import org.apache.lucene.document.NumberTools;
 import org.springframework.util.Assert;
 
 public class MoneyTools {
-    private static final Double _$1 = new Double(1000.0D);
+    private static final Double dou = new Double(1000.0D);
 
     public static String moneyToString(Double paramDouble) {
         Assert.notNull(paramDouble);
-        return String.valueOf(paramDouble.doubleValue() * _$1.doubleValue());
+        return String.valueOf(paramDouble.doubleValue() * dou.doubleValue());
     }
 
     public static Double stringToMoney(String paramString) {
-        Double localDouble = new Double(NumberTools.stringToLong(paramString));
-        return Double.valueOf(localDouble.doubleValue() / _$1.doubleValue());
+        Double localDouble = Double.parseDouble(paramString);
+        return Double.valueOf(localDouble.doubleValue() / dou.doubleValue());
+    }
+
+    public static void main(String[] args){
+        System.out.println(MoneyTools.stringToMoney("3999000.0"));
     }
 }
 
